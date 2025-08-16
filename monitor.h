e0e883a7a0ecf8ca9383b40140bca42b63039455
Divya-Yeduresi <Yeduresi.VenkataNagaDivyasree@bosch.com>
2025-08-16 14:04:41 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -1,3 +1,13 @@
 #pragma once
 
 int vitalsOk(float temperature, float pulseRate, float spo2);
+ 
+// Individual vital check functions (CCN <= 2 each)
+// Returns 1 if vital is OK, 0 if out of range
+int isTemperatureOk(float temperature);    // CCN: 2
+int isPulseRateOk(float pulseRate);        // CCN: 2
+int isSpo2Ok(float spo2);                  // CCN: 2
+ 
+// Alert display function (CCN: 1)
+void displayAlert(const char* message);
+
